fa2ba64eae09e9768327c7f78a759f2ec187fe6e
HakanaiRuby <no>
2020-07-08 17:18:16 +0200

    fix: R.I.P Billy Herrington

--- lib/Components/Book.dart
+++ lib/Components/Book.dart
@@ -19,11 +19,12 @@ List<Book> test = [
   ),
   Book(
     image: "https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg",
-    title: "Semen♂Arsonist",
+    title: "The Arsonist",
+    // title: "Semen♂Arsonist",
   ),
   Book(
     image: "https://www.designforwriters.com/wp-content/uploads/2017/10/design-for-writers-book-cover-tf-2-a-million-to-one.jpg",
-    title: "A Million to One",
+    title: "Million to One",
   ),
   Book(
     image: "https://cdn.pastemagazine.com/www/system/images/photo_albums/best-book-covers-fall-2019/large/bbcdune.jpg?1384968217",
@@ -33,18 +34,18 @@ List<Book> test = [
     image: "https://static.wixstatic.com/media/9c4410_876c178659774d75aa6d9ec9fadfa4a2~mv2_d_1650_2550_s_2.jpg/v1/fill/w_270,h_412,al_c,q_80,usm_0.66_1.00_0.01/WILD%20LIGHT%20EBOOK.webp",
     title: "Wild Light",
   ),
-  Book(
-    image: "https://preview.redd.it/4lg1th3359121.jpg?width=544&auto=webp&s=b930c87f1f34da1629a31d206969bc3fb9c1fb36",
-    title: "Billy Herringt♂n",
-  ),
-  Book(
-    image: "https://preview.redd.it/4lg1th3359121.jpg?width=544&auto=webp&s=b930c87f1f34da1629a31d206969bc3fb9c1fb36",
-    title: "Billy Herringt♂n",
-  ),
-  Book(
-    image: "https://preview.redd.it/4lg1th3359121.jpg?width=544&auto=webp&s=b930c87f1f34da1629a31d206969bc3fb9c1fb36",
-    title: "Billy Herringt♂n",
-  ),
+  // Book(
+  //   image: "https://preview.redd.it/4lg1th3359121.jpg?width=544&auto=webp&s=b930c87f1f34da1629a31d206969bc3fb9c1fb36",
+  //   title: "Billy Herringt♂n",
+  // ),
+  // Book(
+  //   image: "https://preview.redd.it/4lg1th3359121.jpg?width=544&auto=webp&s=b930c87f1f34da1629a31d206969bc3fb9c1fb36",
+  //   title: "Billy Herringt♂n",
+  // ),
+  // Book(
+  //   image: "https://preview.redd.it/4lg1th3359121.jpg?width=544&auto=webp&s=b930c87f1f34da1629a31d206969bc3fb9c1fb36",
+  //   title: "Billy Herringt♂n",
+  // ),
 ];
 
 class BookCard extends StatelessWidget {
