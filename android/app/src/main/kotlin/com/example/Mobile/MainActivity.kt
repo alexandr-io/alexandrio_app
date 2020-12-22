339cae6a4cfc4d0ac32f19a97ef67579d3583392
OrangeCat <no>
2020-12-22 23:41:48 +0100

    del: everything

--- android/app/src/main/kotlin/com/example/Mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/backend/User.dart
+++
@@ -1,8 +0,0 @@
-class User {
-  final String username;
-  final String email;
-  final String authToken;
-  final String refreshToken;
-
-  User({this.username, this.email, this.authToken, this.refreshToken});
-}
