4e08bf001e5fece23bef8fd0a26b4f85bcf517a6
Maxime Houis <maxime.houis@epitech.eu>
2021-01-05 18:35:52 +0100

    fix: release artifacts (#6)
    * other: tmp zip check in actions
    
    * Revert "other: tmp zip check in actions"
    
    This reverts commit c2fadab275816ee36fb66c4cd457fc70bf7fd044.
    
    * test: add release file zipping
    
    * fix: add exec perm to script
    
    * fix: release paths
    
    * other: update release info
    
    * fix: release zip output path

---
+++ ci/prepare_release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ $# < 3 ]]; then
+    echo "usage: $0 <target> <release_name> <paths...>" 1>&2
+    exit 1
+fi
+
+TARGET=$1
+RELEASE_NAME=$2
+
+shift 2
+PATHS=$@
+
+if [[ "$TARGET" == "windows" ]]; then
+    # From https://zaiste.net/posts/how-to-join-elements-of-array-bash/
+    function join { local IFS="$1"; shift; echo "$*"; }
+    
+    set -xe
+
+    PATHS=$(join ',' ${PATHS[@]})
+    echo "Compress-Archive -Path $PATHS -DestinationPath \"$RELEASE_NAME.zip\"" | powershell
+else
+    set -xe
+    zip -r $RELEASE_NAME.zip $PATHS
+fi
